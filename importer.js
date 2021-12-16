e033e10c8a70c0d54c5899e74f08b8f6397ff591
Vishal Sambath <vishal.sambath@quantiphi.com>
2021-12-16 17:31:19 +0530

    stopParsing once error is encountered

--- importer.js
+++ importer.js
@@ -1,12 +1,12 @@
 const csv = require('csv-parser')
 const path = require('path');
 const fs = require('fs')
-const file = 'material_data_final.csv';
+const file = 'materials.csv';
 const results = [];
-const validate=require('./validator')
 
 //=================DB Connection==========================
 const mysql = require('mysql');
+const { exit } = require('process');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -22,29 +22,23 @@ connection.connect((err) => {
 switch (path.extname(file)) {
     case '.csv':
         //===============Inserting CSV values to DB===================
-        let rowCount = 2;
-        let missingValue = false;
         fs.createReadStream(file)
             .pipe(csv())
             .on('data', (data) => {
-                if (Object.keys(data).length != 6) { //check if there are any missing values in csv
-                    missingValue = true;
+                if (Object.keys(data).length != 6) { //stop parsing if there are any missing values in csv
+                    stopParsing();
                 }
                 else {
-                    if (!Number(data.mapping_material_id)) { //check if mapping material id is Integer
-                        console.log('Invalid mapping material ID of CSV');
-                        missingValue = true;
+                    if (!Number(data.mapping_material_id)) { //stop parsing if mapping material id is not an Integer
+                        stopParsing();
                     }
-                    else {
+                    else {//push the line items in an array format
                         results.push([data.material_description, data.brand, data.category, data.pack_size, data.pack_type, parseInt(data.mapping_material_id)])
                     }
                 }
             })
             .on('end', () => {
-                if (!missingValue) {
                     insertRows(results);
-                    console.log('Inserted rows')
-                }
             })
         // ============================================================
         break;
@@ -55,33 +49,27 @@ switch (path.extname(file)) {
         fs.readFile(file, function (err, data) {
             // Check for errors
             if (err) throw err;
-
             // Converting to JSON
             const materials = JSON.parse(data);
-            let missingValue = false;
             for (let i in materials) {
                 // console.log(i)
-                if (!materials[i].material_description || !materials[i].mapping_material_id) {//check if any mandatory fields are missing
-                    console.log('Missing important key values')
+                if (!materials[i].mapping_material_id) {//check if any mandatory fields are missing
+                    stopParsing();
                 }
                 else {
                     if (!Number(data.mapping_material_id)) {// check if mapping material id is Integer
-                        console.log("Invalid mapping material ID")
-                        missingValue = true;
+                        stopParsing();
                     }
-                    else {
+                    else {//push the json object properties in an array format
                         results.push([materials[i].material_description, materials[i].brand, materials[i].category, materials[i].pack_size, materials[i].pack_type, parseInt(materials[i].mapping_material_id)])
                     }
                 }
             }
-            if (!missingValue) {
-                insertRows(results);
-                console.log('Done');
-            }
+            insertRows(results);
+            
         })
         //=====================================
         break;
-
 
     default:
         console.log('File extension not supported! Please provide data in .csv or .json format');
@@ -91,4 +79,10 @@ switch (path.extname(file)) {
 function insertRows(arr) {
     let sql = "INSERT INTO materials ( material_description,brand,category,pack_size,pack_type,mapping_material_id) VALUES ?";
     connection.query(sql, [arr]);
+    console.log('Inserted rows')
+}
+
+function stopParsing(){
+    console.log(" Program terminated due to error while parsing");
+    exit();
 }
